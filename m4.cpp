cd632905fdc85cd256faf0430189279555bb79fa
himanta1012 <50390378+himanta1012@users.noreply.github.com>
2019-10-10 23:18:09 +0530

    updated M4.cpp

--- m4.cpp
+++ m4.cpp
@@ -1,14 +1,12 @@
 #include<iostream>
 #include<stdio.h>
- 
-using namespace std;
+ using namespace std;
 void len(char st[])
 {	int i;
     for(i=0;st[i]!='\0';++i);
 	
 	cout<<"\nLength of the string is "<<i;
 }
-
 int main()
 {
 	char str[100];
